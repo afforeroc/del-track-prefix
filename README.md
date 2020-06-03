661d1aef220349b2f0126bc4e22e7437b982d3a9
Andres Felipe Forero Correa <afforeroc@unal.edu.co>
2020-06-03 01:50:53 -0500

    First commit / Were added the most common track number prefixes

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # del-track-prefix
 Script tool to delete track prefix of music files
+
+## Run
+1. For Windows CMD
+```
+bash zdelpt.sh
+```
+2. For Linux Terminal
+```
+sh bash zdelpt.sh
+````
---
+++ zdelpt.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Remove the most common track number prefixes for music and sound files"
+
+# One digit
+echo "Removing prefix: [0-9][space][hyphen][space]"
+for f in ./*.*; do mv "$f" "${f/[0-9] - //}"; done; sleep 2
+echo ""
+echo "Removing [0-9][dot][space]"
+for f in ./*.*; do mv "$f" "${f/[0-9]. //}"; done; sleep 2
+echo ""
+echo "Removing prefix: [0-9][dot]"
+for f in ./*.*; do mv "$f" "${f/[0-9].//}"; done; sleep 2
+echo ""
+echo "Removing [0-9][underscore]"
+for f in ./*.*; do mv "$f" "${f/[0-9]_//}"; done; sleep 2
+echo ""
+
+# Two digits
+echo "Removing prefix: [0-9][0-9][space][hyphen][space]"
+for f in ./*.*; do mv "$f" "${f/[0-9][0-9] - //}"; done; sleep 2
+echo ""
+echo "Removing [0-9][0-9][dot][space]"
+for f in ./*.*; do mv "$f" "${f/[0-9][0-9]. //}"; done; sleep 2
+echo ""
+echo "Removing prefix: [0-9][0-9][dot]"
+for f in ./*.*; do mv "$f" "${f/[0-9][0-9].//}"; done; sleep 2
+echo ""
+echo "Removing [0-9][0-9][underscore]"
+for f in ./*.*; do mv "$f" "${f/[0-9][0-9]_//}"; done; sleep 2
+echo ""
+echo "Done"
