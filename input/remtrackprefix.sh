47e9539d7b2d6d48da293854c5161049704016a0
Andres Felipe Forero Correa <afforeroc@unal.edu.co>
2020-06-04 01:54:34 -0500

    Some exceptions were added

--- input/remtrackprefix.sh
+++ input/remtrackprefix.sh
@@ -2,30 +2,36 @@
 echo "For all files:"
 
 # One digit
-echo "Removing prefix: [0-9][space][hyphen][space]"
-for f in ./*.*; do mv "$f" "${f/[0-9] - //}"; done; sleep 2
-echo ""
 echo "Removing [0-9][dot][space]"
-for f in ./*.*; do mv "$f" "${f/[0-9]. //}"; done; sleep 2
+for f in ./*.*; do mv "$f" "${f/[0-9]. //}"; done;
 echo ""
 echo "Removing prefix: [0-9][dot]"
-for f in ./*.*; do mv "$f" "${f/[0-9].//}"; done; sleep 2
+for f in ./*.*; do mv "$f" "${f/[0-9].//}"; done;
+echo ""
+echo "Removing prefix: [0-9][space][hyphen][space]"
+for f in ./*.*; do mv "$f" "${f/[0-9] - //}"; done;
 echo ""
 echo "Removing [0-9][underscore]"
-for f in ./*.*; do mv "$f" "${f/[0-9]_//}"; done; sleep 2
+for f in ./*.*; do mv "$f" "${f/[0-9]_//}"; done;
+echo ""
+echo "Removing [0-9][hyphen]"
+for f in ./*.*; do mv "$f" "${f/[0-9]-//}"; done;
 echo ""
 
 # Two digits
-echo "Removing prefix: [0-9][0-9][space][hyphen][space]"
-for f in ./*.*; do mv "$f" "${f/[0-9][0-9] - //}"; done; sleep 2
-echo ""
 echo "Removing [0-9][0-9][dot][space]"
-for f in ./*.*; do mv "$f" "${f/[0-9][0-9]. //}"; done; sleep 2
+for f in ./*.*; do mv "$f" "${f/[0-9][0-9]. //}"; done;
 echo ""
 echo "Removing prefix: [0-9][0-9][dot]"
-for f in ./*.*; do mv "$f" "${f/[0-9][0-9].//}"; done; sleep 2
+for f in ./*.*; do mv "$f" "${f/[0-9][0-9].//}"; done;
+echo ""
+echo "Removing prefix: [0-9][0-9][space][hyphen][space]"
+for f in ./*.*; do mv "$f" "${f/[0-9][0-9] - //}"; done;
 echo ""
 echo "Removing [0-9][0-9][underscore]"
-for f in ./*.*; do mv "$f" "${f/[0-9][0-9]_//}"; done; sleep 2
+for f in ./*.*; do mv "$f" "${f/[0-9][0-9]_//}"; done;
+echo ""
+echo "Removing [0-9][0-9][hyphen]"
+for f in ./*.*; do mv "$f" "${f/[0-9][0-9]-//}"; done;
 echo ""
 echo "Done"
